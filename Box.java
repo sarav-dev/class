97fca06ad7df1a7a681dc1f02425de6a2f68c31d
unknown <webdsnr_sarav@hotmail.com>
2023-02-09 23:31:02 +0530

    first commit

---
+++ Box.java
@@ -0,0 +1,15 @@
+class Box{
+
+    int width;
+
+    int height;
+
+    int length;
+
+    void volume(){
+
+        System.out.println(length * width * height);
+
+    }
+
+}
---
+++ BoxUse.java
@@ -0,0 +1,15 @@
+public class BoxUse {
+
+    public static void main(String[] args) {
+
+        Box b = new Box();
+
+        b.height = 5;
+
+        b.width = 4;
+
+        b.volume();
+
+    }
+
+}
